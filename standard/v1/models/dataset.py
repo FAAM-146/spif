2ad954b824b5dce530a2771af4ce54d6a43f0e7b
Graeme Nott <graeme.nott@faam.ac.uk>
2023-12-15 16:55:23 +0000

    Updated dataset meta tags, short_name to mandatory and canonical_name to long_name

--- standard/v1/models/dataset.py
+++ standard/v1/models/dataset.py
@@ -15,7 +15,8 @@ from .variable import Variable
 
 class DatasetMeta(BaseModel):
     file_pattern: str = Field(description='Canonical filename pattern for this dataset')
-    short_name: Optional[str] = Field(description='Unique hort name for this dataset', default=None)
+    short_name: str = Field(description='Unique short name for this dataset')
+    long_name: Optional[str] = Field(description='Long descriptive name for this dataset', default=None)
     description: Optional[str] = Field(description='Description of this dataset', default=None)
     references: Optional[list[tuple[str, str]]] = Field(description='References for this dataset', default=None)
 
