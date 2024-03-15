b4c46693a387370bffbda6b9f10026ce522d23a6
Graeme Nott <graeme.nott@faam.ac.uk>
2024-03-15 09:47:55 +0000

    Added version info--not sure about this

--- standard/v1/__init__.py
+++ standard/v1/__init__.py
@@ -1,3 +1,9 @@
 from . import models
 from . import attributes
 from . import defaults
+
+# Use semantic-version package?
+__version_major__ = 1
+__version_minor__ = 0
+
+__version__ = f'{__version_major__}.{__version_minor__}'
