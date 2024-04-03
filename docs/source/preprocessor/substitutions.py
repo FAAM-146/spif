054e47f482f58037c20a3023d4d3b22ec1634209
Graeme Nott <graeme.nott@faam.ac.uk>
2024-04-03 17:20:32 +0100

    Corrected the kwargs

--- docs/source/preprocessor/substitutions.py
+++ docs/source/preprocessor/substitutions.py
@@ -9,26 +9,25 @@ __all__ = ['rst_substitutions']
 section_markers = ['=', '-', '^', '"']
 
 
-def _level_rst(level=0) -> str:
+def _level_rst(level=0, **kwargs) -> str:
 
     return section_markers[clip(level, 0, 3)]
 
 
-def _level_subs(level=0) -> str:
+def _level_subs(level=0, **kwargs) -> str:
 
     text = ""
 
-    text += f".. |tab|: replace:: {'  ' * level}"
-    text += f".. |sec|: replace:: {_level_rst(level)}"
+    text += f'.. |tab|: replace:: {"  " * level}\n'
+    text += f'.. |sec|: replace:: {_level_rst(level)}\n'
 
     return text
 
 
-def rst_substitutions(**kwargs) -> str:
+def rst_substitutions(level=0, **kwargs) -> str:
 
     text = ""
-
-    text += _level_subs(kwargs.get(level, None))
+    text += _level_subs(level)
 
     return text
 
