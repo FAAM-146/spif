c4ca9ac13ea5e0a1fbf1970d9864880de99320f6
Graeme Nott <graeme.nott@faam.ac.uk>
2024-04-04 12:16:22 +0100

    Added css paths and changed theme to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,16 +102,24 @@ add_module_names = True
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# A string of reStructuredText that will be included at the beginning
+# of every source file that is read.
+rst_prolog = """
+ .. include:: <s5defs.txt>
+
+ """
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ['css/custom.css', 'css/mods.css']
